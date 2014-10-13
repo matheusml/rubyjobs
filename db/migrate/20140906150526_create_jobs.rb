db5c8efea34a2714c34b4279beac3902b0597dac
Jhony Lucas <jhonyjl37@gmail.com>
2014-10-13 06:39:25 -0300

    Botão do twitter adicionado. #18

--- db/migrate/20140906150526_create_jobs.rb
+++ db/migrate/20140906150526_create_jobs.rb
@@ -1,13 +1,13 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-    	t.string :title
-    	t.string :location
-    	t.string :company
-    	t.string :email
-    	
-    	t.text :skills
-    	t.text :description
+      t.string :title
+      t.string :location
+      t.string :company
+      t.string :email
+
+      t.text :skills
+      t.text :description
 
       t.timestamps
     end
